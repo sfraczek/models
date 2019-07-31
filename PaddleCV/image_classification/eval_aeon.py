83ea48e0a94ebd1aa920b900358a16feea7edaa6
Adam Rogowiec <adam.rogowiec@intel.com>
2019-07-31 17:00:47 +0200

    Update eval_aeon script to release/1.5

--- PaddleCV/image_classification/eval_aeon.py
+++ PaddleCV/image_classification/eval_aeon.py
@@ -1,19 +1,34 @@
+#copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import os
-import numpy as np
 import time
 import sys
+import math
+import numpy as np
+import argparse
+import functools
+
 import paddle
 import paddle.fluid as fluid
 import reader_aeon as reader
-import argparse
-import functools
 import models
 from utils.learning_rate import cosine_decay
-from utils.utility import add_arguments, print_arguments
-import math
+from utils.utility import add_arguments, print_arguments, check_gpu
 
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
@@ -122,6 +137,7 @@ def eval(args):
 def main():
     args = parser.parse_args()
     print_arguments(args)
+    check_gpu(args.use_gpu)
     eval(args)
 
 
