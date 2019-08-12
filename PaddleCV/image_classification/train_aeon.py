d3916566d762d6c0464d835dcf3411fddc3ccfad
Adam Rogowiec <adam.rogowiec@intel.com>
2019-08-12 18:58:16 +0200

    Update aeon train scripts to release/1.5

--- PaddleCV/image_classification/train_aeon.py
+++ PaddleCV/image_classification/train_aeon.py
@@ -1,22 +1,53 @@
+#copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 import os
 import numpy as np
 import time
 import sys
 import functools
 import math
-import paddle
-import paddle.fluid as fluid
-import reader_aeon as reader
+
+
+def set_paddle_flags(flags):
+    for key, value in flags.items():
+        if os.environ.get(key, None) is None:
+            os.environ[key] = str(value)
+
+
+# NOTE(paddle-dev): All of these flags should be
+# set before `import paddle`. Otherwise, it would
+# not take any effect.
+set_paddle_flags({
+    'FLAGS_eager_delete_tensor_gb': 0,  # enable gc
+    'FLAGS_fraction_of_gpu_memory_to_use': 0.98
+})
 import argparse
 import functools
 import subprocess
+import paddle
+import paddle.fluid as fluid
+import paddle.dataset.flowers as flowers
+import reader_aeon as reader
 import utils
 import models
 from utils.fp16_utils import create_master_params_grads, master_param_to_train_param
-from utils.utility import add_arguments, print_arguments
+from utils.utility import add_arguments, print_arguments, check_gpu
 from utils.learning_rate import cosine_decay_with_warmup
 
 IMAGENET1000 = 1281167
@@ -32,12 +63,14 @@ add_arg('num_epochs',       int,   120,                  "number of epochs.")
 add_arg('class_dim',        int,   1000,                 "Class number.")
 add_arg('image_shape',      str,   "3,224,224",          "input image size")
 add_arg('model_save_dir',   str,   "output",             "model save directory")
-add_arg('with_mem_opt',     bool,  True,                 "Whether to use memory optimization or not.")
+add_arg('with_mem_opt',     bool,  False,                 "Whether to use memory optimization or not.")
+add_arg('with_inplace',     bool,  True,                 "Whether to use inplace memory optimization.")
 add_arg('pretrained_model', str,   None,                 "Whether to use pretrained model.")
 add_arg('checkpoint',       str,   None,                 "Whether to resume checkpoint.")
 add_arg('lr',               float, 0.1,                  "set learning rate.")
 add_arg('lr_strategy',      str,   "piecewise_decay",    "Set the learning rate decay strategy.")
 add_arg('model',            str,   "SE_ResNeXt50_32x4d", "Set the network to use.")
+add_arg('enable_ce',        bool,  False,                "If set True, enable continuous evaluation job.")
 add_arg('data_dir',         str,   "./data/ILSVRC2012/",  "The ImageNet dataset root dir.")
 add_arg('fp16',             bool,  False,                "Enable half precision training with fp16." )
 add_arg('scale_loss',       float, 1.0,                  "Scale loss for fp16." )
@@ -49,8 +82,13 @@ add_arg('lower_scale',      float,     0.08,      "Set the lower_scale in ramdom
 add_arg('lower_ratio',      float,     3./4.,      "Set the lower_ratio in ramdom_crop")
 add_arg('upper_ratio',      float,     4./3.,      "Set the upper_ratio in ramdom_crop")
 add_arg('resize_short_size',      int,     256,      "Set the resize_short_size")
+add_arg('use_mixup',      bool,      False,        "Whether to use mixup or not")
+add_arg('mixup_alpha',      float,     0.2,      "Set the mixup_alpha parameter")
 add_arg('is_distill',       bool,  False,        "is distill or not")
-
+add_arg('dummy_data',      bool,     False,      "Use dummy data reader.")
+add_arg('cache_dir',           str, "",              "Place where aeon will store cache")
+add_arg('reader_thread_count', int, 12,              "How many threads to allocate for reader")
+add_arg('random_seed',         int, 0,               "Random seed. Choose 0 for non-deterministic.")
 
 def optimizer_setting(params):
     ls = params["learning_strategy"]
@@ -173,12 +211,17 @@ def calc_loss(epsilon,label,class_dim,softmax_out,use_label_smoothing):
 
 def net_config(image, model, args, is_train, label=0, y_a=0, y_b=0, lam=0.0):
     model_list = [m for m in dir(models) if "__" not in m]
-    assert args.model in model_list, "{} is not lists: {}".format(args.model,
-                                                                  model_list)
+    assert args.model in model_list, "{} is not lists: {}".format(args.model, model_list)
     class_dim = args.class_dim
     model_name = args.model
+    use_mixup = args.use_mixup
     use_label_smoothing = args.use_label_smoothing
     epsilon = args.label_smoothing_epsilon
+
+    if args.enable_ce:
+        assert model_name == "SE_ResNeXt50_32x4d"
+        model.params["dropout_seed"] = 100
+        class_dim = 102
 
     if model_name == "GoogleNet":
         out0, out1, out2 = model.net(input=image, class_dim=class_dim)
@@ -198,7 +241,19 @@ def net_config(image, model, args, is_train, label=0, y_a=0, y_b=0, lam=0.0):
             out = model.net(input=image, class_dim=class_dim)
             softmax_out = fluid.layers.softmax(out, use_cudnn=False)
             if is_train:
-                cost = calc_loss(epsilon,label,class_dim,softmax_out,use_label_smoothing)
+                if use_mixup:
+                    loss_a = calc_loss(epsilon,y_a,class_dim,softmax_out,use_label_smoothing)
+                    loss_b = calc_loss(epsilon,y_b,class_dim,softmax_out,use_label_smoothing)
+                    loss_a_mean = fluid.layers.mean(x = loss_a)
+                    loss_b_mean = fluid.layers.mean(x = loss_b)
+                    cost = lam * loss_a_mean + (1 - lam) * loss_b_mean
+                    avg_cost = fluid.layers.mean(x=cost)
+                    if args.scale_loss > 1:
+                        avg_cost = fluid.layers.mean(x=cost) * float(args.scale_loss)
+                    return avg_cost
+                else:
+                    cost = calc_loss(epsilon,label,class_dim,softmax_out,use_label_smoothing)
+
             else:
                 cost = fluid.layers.cross_entropy(input=softmax_out, label=label)
         else:
@@ -224,18 +279,40 @@ def build_program(is_train, main_prog, startup_prog, args, place):
                                                                      model_list)
     model = models.__dict__[model_name]()
     with fluid.program_guard(main_prog, startup_prog):
+        use_mixup = args.use_mixup
+        if is_train and use_mixup:
+            py_reader = fluid.layers.py_reader(
+                capacity=16,
+                shapes=[[-1] + image_shape, [-1, 1], [-1, 1], [-1, 1]],
+                lod_levels=[0, 0, 0, 0],
+                dtypes=["float32", "int64", "int64", "float32"],
+                use_double_buffer=True)
+        else:
+            py_reader = fluid.layers.py_reader(
+                capacity=16,
+                shapes=[[-1] + image_shape, [-1, 1]],
+                lod_levels=[0, 0],
+                dtypes=["float32", "int64"],
+                use_double_buffer=True)
 
         with fluid.unique_name.guard():
-            image_shape = [int(m) for m in args.image_shape.split(",")]
-            image = fluid.layers.data(name='image', shape=image_shape, dtype='float32')
-            label = fluid.layers.data(name='label', shape=[1], dtype='int64')
-            feeder = fluid.DataFeeder(place=place, feed_list=[image, label])
+            if is_train and  use_mixup:
+                image, y_a, y_b, lam = fluid.layers.read_file(py_reader)
+                if args.fp16:
+                    image = fluid.layers.cast(image, "float16")
+                avg_cost = net_config(image=image, y_a=y_a, y_b=y_b, lam=lam, model=model, args=args, label=0, is_train=True)
+                avg_cost.persistable = True
+                build_program_out = [py_reader, avg_cost]
+            else:
+                image, label = fluid.layers.read_file(py_reader)
+                if args.fp16:
+                    image = fluid.layers.cast(image, "float16")
+                avg_cost, acc_top1, acc_top5 = net_config(image, model, args, label=label, is_train=is_train)
+                avg_cost.persistable = True
+                acc_top1.persistable = True
+                acc_top5.persistable = True
+                build_program_out = [py_reader, avg_cost, acc_top1, acc_top5]
 
-            avg_cost, acc_top1, acc_top5 = net_config(image, model, args, label=label, is_train=is_train)
-            avg_cost.persistable = True
-            acc_top1.persistable = True
-            acc_top5.persistable = True
-            build_program_out = [feeder, avg_cost, acc_top1, acc_top5]
             if is_train:
                 params = model.params
                 params["total_images"] = args.total_images
@@ -249,6 +326,7 @@ def build_program(is_train, main_prog, startup_prog, args, place):
                 optimizer = optimizer_setting(params)
                 optimizer.minimize(avg_cost)
                 global_lr = optimizer._global_learning_rate()
+                global_lr.persistable=True
                 build_program_out.append(global_lr)
 
     return build_program_out
@@ -270,6 +348,7 @@ def train(args):
     pretrained_model = args.pretrained_model
     with_memory_optimization = args.with_mem_opt
     model_save_dir = args.model_save_dir
+    use_mixup = args.use_mixup
 
     startup_prog = fluid.Program()
     train_prog = fluid.Program()
@@ -281,10 +360,14 @@ def train(args):
                      is_train=True,
                      main_prog=train_prog,
                      startup_prog=startup_prog,
-                     args=args,
-                     place=place)
-    feeder, train_cost, train_acc1, train_acc5, global_lr = b_out[0],b_out[1],b_out[2],b_out[3], b_out[4]
-    train_fetch_list = [train_cost.name, train_acc1.name, train_acc5.name, global_lr.name]
+                     args=args)
+    if use_mixup:
+        train_py_reader, train_cost, global_lr = b_out[0], b_out[1], b_out[2]
+        train_fetch_list = [train_cost.name, global_lr.name]
+
+    else:
+        train_py_reader, train_cost, train_acc1, train_acc5, global_lr = b_out[0],b_out[1],b_out[2],b_out[3],b_out[4]
+        train_fetch_list = [train_cost.name, train_acc1.name, train_acc5.name, global_lr.name]
 
     b_out_test = build_program(
                      is_train=False,
@@ -320,16 +403,45 @@ def train(args):
     train_batch_size = args.batch_size / device_num
 
     test_batch_size = 16
-    train_reader = reader.train_reader(settings=args)
-    #  test_reader = reader.val_reader(settings=args)
+    if not args.enable_ce:
+        train_reader = reader.train(settings=args,
+                                    batch_size=train_batch_size,
+                                    drop_last=True)
+        test_reader = reader.val(settings=args, batch_size=test_batch_size)
+    else:
+        # use flowers dataset for CE and set use_xmap False to avoid disorder data
+        # but it is time consuming. For faster speed, need another dataset.
+        import random
+        random.seed(0)
+        np.random.seed(0)
+        train_reader = paddle.batch(
+            flowers.train(use_xmap=False),
+            batch_size=train_batch_size,
+            drop_last=True)
+        test_reader = paddle.batch(
+            flowers.test(use_xmap=False), batch_size=test_batch_size)
+
+    train_py_reader.decorate_paddle_reader(train_reader)
+    test_py_reader.decorate_paddle_reader(test_reader)
 
     # use_ngraph is for CPU only, please refer to README_ngraph.md for details
     use_ngraph = os.getenv('FLAGS_use_ngraph')
     if not use_ngraph:
+        build_strategy = fluid.BuildStrategy()
+        # memopt may affect GC results
+        #build_strategy.memory_optimize = args.with_mem_opt
+        build_strategy.enable_inplace = args.with_inplace
+        #build_strategy.fuse_all_reduce_ops=1
+
+        exec_strategy = fluid.ExecutionStrategy()
+        exec_strategy.num_iteration_per_drop_scope = 10
+
         train_exe = fluid.ParallelExecutor(
             main_program=train_prog,
             use_cuda=bool(args.use_gpu),
-            loss_name=train_cost.name)
+            loss_name=train_cost.name,
+            build_strategy=build_strategy,
+            exec_strategy=exec_strategy)
     else:
         train_exe = exe
 
@@ -348,50 +460,62 @@ def train(args):
         test_info = [[], [], []]
         train_time = []
         batch_id = 0
-        max_iter = math.floor(args.total_images/args.batch_size)
-        while batch_id < max_iter:
-            batch_id += 1
-            data = train_reader.next()
-            batch = {k: v for k, v in data}
-            images = batch['image']
-            labels = batch['label']
+        time_record=[]
+        try:
+            while True:
+                t1 = time.time()
+                if use_mixup:
+                    if use_ngraph:
+                        loss, lr = train_exe.run(train_prog, fetch_list=train_fetch_list)
+                    else:
+                        loss, lr = train_exe.run(fetch_list=train_fetch_list)
 
-            images /= 255
-            images -= img_mean
-            images /= img_std
-            feed_data = zip(images, labels)
-            t1 = time.time()
-            if use_ngraph:
-                loss, acc1, acc5, lr = train_exe.run(train_prog, fetch_list=train_fetch_list, feed=feeder.feed(feed_data))
-            else:
-                loss, acc1, acc5, lr = train_exe.run(fetch_list=train_fetch_list, feed=feeder.feed(feed_data))
+                    t2 = time.time()
+                else:
+                    if use_ngraph:
+                        loss, acc1, acc5, lr = train_exe.run(train_prog, fetch_list=train_fetch_list)
+                    else:
+                        loss, acc1, acc5, lr = train_exe.run(fetch_list=train_fetch_list)
 
-            acc1 = np.mean(np.array(acc1))
-            acc5 = np.mean(np.array(acc5))
-            train_info[1].append(acc1)
-            train_info[2].append(acc5)
+                    t2 = time.time()
+                    acc1 = np.mean(np.array(acc1))
+                    acc5 = np.mean(np.array(acc5))
+                    train_info[1].append(acc1)
+                    train_info[2].append(acc5)
 
-            t2 = time.time()
-            period = t2 - t1
+                period = t2 - t1
+                time_record.append(period)
 
-            loss = np.mean(np.array(loss))
-            train_info[0].append(loss)
-            lr = np.mean(np.array(lr))
-            train_time.append(period)
+                loss = np.mean(np.array(loss))
+                train_info[0].append(loss)
+                lr = np.mean(np.array(lr))
+                train_time.append(period)
 
-            if batch_id % 10 == 0:
-                print("Pass {0}, trainbatch {1}, loss {2}, \
-                    acc1 {3}, acc5 {4}, lr {5}, time {6}"
-                        .format(pass_id, batch_id, "%.5f"%loss, "%.5f"%acc1, "%.5f"%acc5, "%.5f" %
-                                lr, "%2.2f sec" % period))
-                sys.stdout.flush()
+                if batch_id % 10 == 0:
+                    period = np.mean(time_record)
+                    time_record=[]
+                    img_per_sec = args.batch_size / period
+
+                    if use_mixup:
+                        print("Pass {0}, trainbatch {1}, loss {2}, lr {3}, time {4}, img/s {}"
+                              .format(pass_id, batch_id, "%.5f"%loss, "%.5f" % lr,
+                                      "%2.2f sec" % period, "%2.2f" % img_per_sec))
+                    else:
+                        print("Pass {0}, trainbatch {1}, loss {2}, acc1 {3}, acc5 {4}, lr {5}, "
+                              " time {6}, img/s {7}".format(
+                                pass_id, batch_id, "%.5f"%loss, "%.5f"%acc1, "%.5f"%acc5,
+                                "%.5f" % lr, "%2.2f sec" % period, "%2.2f" % img_per_sec))
+                    sys.stdout.flush()
+                batch_id += 1
+        except fluid.core.EOFException:
+            train_py_reader.reset()
 
         train_loss = np.array(train_info[0]).mean()
-        train_acc1 = np.array(train_info[1]).mean()
-        train_acc5 = np.array(train_info[2]).mean()
-        train_speed = np.array(train_time).mean() / (train_batch_size *
-                                                     device_num)
-
+        if not use_mixup:
+            train_acc1 = np.array(train_info[1]).mean()
+            train_acc5 = np.array(train_info[2]).mean()
+        train_speed = np.array(train_time).mean() / (train_batch_size * device_num)
+        test_py_reader.start()
 
         test_batch_id = 0
         while False:
@@ -429,10 +553,15 @@ def train(args):
         test_acc1 = np.array(test_info[1]).mean()
         test_acc5 = np.array(test_info[2]).mean()
 
-        print("End pass {0}, train_loss {1}, train_acc1 {2}, train_acc5 {3}, "
-                "test_loss {4}, test_acc1 {5}, test_acc5 {6}".format(
-                    pass_id, "%.5f"%train_loss, "%.5f"%train_acc1, "%.5f"%train_acc5, "%.5f"%test_loss,
-                    "%.5f"%test_acc1, "%.5f"%test_acc5))
+        if use_mixup:
+            print("End pass {0}, train_loss {1}, test_loss {2}, test_acc1 {3}, test_acc5 {4}".format(
+                      pass_id, "%.5f"%train_loss, "%.5f"%test_loss, "%.5f"%test_acc1, "%.5f"%test_acc5))
+        else:
+
+            print("End pass {0}, train_loss {1}, train_acc1 {2}, train_acc5 {3}, "
+                  "test_loss {4}, test_acc1 {5}, test_acc5 {6}".format(
+                      pass_id, "%.5f"%train_loss, "%.5f"%train_acc1, "%.5f"%train_acc5, "%.5f"%test_loss,
+                      "%.5f"%test_acc1, "%.5f"%test_acc5))
         sys.stdout.flush()
 
         model_path = os.path.join(model_save_dir + '/' + model_name,
@@ -447,6 +576,7 @@ def train(args):
 def main():
     args = parser.parse_args()
     print_arguments(args)
+    check_gpu(args.use_gpu)
     train(args)
 
 
