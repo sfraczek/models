6b278cb2f184aca7e717764bdf247a0cc6594d61
Adam Rogowiec <adam.rogowiec@intel.com>
2019-08-12 18:58:27 +0200

    Update eval_aeon script to release/1.5

--- PaddleCV/image_classification/eval_aeon.py
+++ PaddleCV/image_classification/eval_aeon.py
@@ -1,19 +1,34 @@
+#copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import os
-import numpy as np
 import time
 import sys
+import math
+import numpy as np
+import argparse
+import functools
+
 import paddle
 import paddle.fluid as fluid
 import reader_aeon as reader
-import argparse
-import functools
 import models
 from utils.learning_rate import cosine_decay
-from utils.utility import add_arguments, print_arguments
-import math
+from utils.utility import add_arguments, print_arguments, check_gpu
 
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
@@ -26,9 +41,11 @@ add_arg('with_mem_opt',     bool, True,                "Whether to use memory op
 add_arg('pretrained_model', str,  None,                "Whether to use pretrained model.")
 add_arg('model',            str,  "SE_ResNeXt50_32x4d", "Set the network to use.")
 add_arg('resize_short_size', int, 256,                "Set resize short size")
-add_arg('iterations',       int, 50000,                  "Quit after this many iterations")
+add_arg('data_dir',            str, "./data/ILSVRC2012/", "The ImageNet dataset root dir.")
+add_arg('cache_dir',           str, "",                   "Place where aeon will store cache")
+add_arg('reader_thread_count', int, 12,                   "How many threads to allocate for reader")
+add_arg('random_seed',         int, 0,                    "Random seed. Choose 0 for non-deterministic.")
 # yapf: enable
-
 
 def eval(args):
     # parameters from arguments
@@ -39,8 +56,8 @@ def eval(args):
     image_shape = [int(m) for m in args.image_shape.split(",")]
 
     model_list = [m for m in dir(models) if "__" not in m]
-    assert model_name in model_list, "{} is not in lists: {}".format(
-        args.model, model_list)
+    assert model_name in model_list, "{} is not in lists: {}".format(args.model,
+                                                                     model_list)
 
     image = fluid.layers.data(name='image', shape=image_shape, dtype='float32')
     label = fluid.layers.data(name='label', shape=[1], dtype='int64')
@@ -79,38 +96,19 @@ def eval(args):
     exe = fluid.Executor(place)
     exe.run(fluid.default_startup_program())
 
+
     fluid.io.load_persistables(exe, pretrained_model)
 
-    val_reader = reader.val_reader(settings=args)
+    val_reader = reader.val(settings=args, batch_size=args.batch_size)
     feeder = fluid.DataFeeder(place=place, feed_list=[image, label])
 
     test_info = [[], [], []]
     cnt = 0
-    batch_id = 0
-    img_mean = np.array([0.485, 0.456, 0.406]).reshape((3, 1, 1))
-    img_std = np.array([0.229, 0.224, 0.225]).reshape((3, 1, 1))
-    img_mean = np.array(img_mean).reshape((3, 1, 1))
-    img_std = np.array(img_std).reshape((3, 1, 1))
-    while True:
-        batch_id += 1
-        data = val_reader.next()
-        batch = {k: v for k, v in data}
-        images = batch['image']
-        labels = batch['label']
-
-        images /= 255
-        images -= img_mean
-        images /= img_std
-
+    for batch_id, data in enumerate(val_reader()):
         t1 = time.time()
-        feed_data = zip(images, labels)
-        loss, acc1, acc5 = exe.run(
-            test_program, fetch_list=fetch_list, feed=feeder.feed(feed_data))
-        #  with open('objs_aeon.txt', 'w') as f:
-        #      print(len(feed_data))
-        #      np.set_printoptions(threshold=np.inf)
-        #      print(feed_data[0])
-        #      return
+        loss, acc1, acc5 = exe.run(test_program,
+                                   fetch_list=fetch_list,
+                                   feed=feeder.feed(data))
         t2 = time.time()
         period = t2 - t1
         loss = np.mean(loss)
@@ -127,21 +125,19 @@ def eval(args):
                   "%2.2f sec" % period))
             sys.stdout.flush()
 
-        if batch_id == args.iterations:
-            break
-
     test_loss = np.sum(test_info[0]) / cnt
     test_acc1 = np.sum(test_info[1]) / cnt
     test_acc5 = np.sum(test_info[2]) / cnt
 
     print("Test_loss {0}, test_acc1 {1}, test_acc5 {2}".format(
-        "%.5f" % test_loss, "%.5f" % test_acc1, "%.5f" % test_acc5))
+        "%.5f"%test_loss, "%.5f"%test_acc1, "%.5f"%test_acc5))
     sys.stdout.flush()
 
 
 def main():
     args = parser.parse_args()
     print_arguments(args)
+    check_gpu(args.use_gpu)
     eval(args)
 
 
