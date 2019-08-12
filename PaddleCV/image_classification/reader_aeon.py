903148776675567db2c74d401251d20e1e7e055f
Adam Rogowiec <adam.rogowiec@intel.com>
2019-08-12 18:57:59 +0200

    Pass data dir to aeon reader as an argument.

--- PaddleCV/image_classification/reader_aeon.py
+++ PaddleCV/image_classification/reader_aeon.py
@@ -1,41 +1,43 @@
 # coding: utf8
-import os
-import functools
-import numpy as np
-import paddle
-import io
-
-import json
 from aeon import DataLoader
+import json
+import os
 
-RANDOM_SEED = 1  # setting to 0 should yields random random seed (non deterministic)
-THREAD = 12
-DATA_DIR = "data/ILSVRC2012"
-VAL_LIST_DIR = "data/ILSVRC2012/val-index.tsv"
-TRAIN_LIST_DIR = "data/ILSVRC2012/train-index.tsv"
-CACHE_DIR = "/mnt/drive/.aeon-cache/"
-
-# dane wchodza rgb
-# co robi ten transpose 2,0,1
-# bgr -> rgb
-# hwc -> chw
-# subtract mean div by stdev
-# img_mean = np.array([0.485, 0.456, 0.406]).reshape((3, 1, 1))
-# img_std = np.array([0.229, 0.224, 0.225]).reshape((3, 1, 1))
+VAL_LIST = "val-index.tsv"
+TRAIN_LIST = "train-index.tsv"
+MEAN = [0.485, 0.456, 0.406]
+STDDEV = [0.229, 0.224, 0.225]
 
 
-def train_reader(settings):
+def common_config(cache_dir, data_dir, thread_count, random_seed):
     image_config = {
         "type": "image",
         "height": 224,
         "width": 224,
         "channels": 3,
+        "mean": MEAN,
+        "stddev": STDDEV,
         "output_type": "float",
         "channel_major": True,
         "bgr_to_rgb": True
     }
 
     label_config = {"type": "label", "binary": False}
+
+    config = dict()
+    config['random_seed'] = random_seed
+    config['decode_thread_count'] = thread_count
+    config['manifest_root'] = data_dir
+    config['cache_directory'] = cache_dir
+    config['etl'] = [image_config, label_config]
+    config['iteration_mode'] = "ONCE"
+
+    return config
+
+
+def train_reader(settings, batch_size):
+    config = common_config(settings.cache_dir, settings.data_dir,
+                           settings.reader_thread_count, settings.random_seed)
 
     augmentation_config = {
         "random_seed": 1,
@@ -47,43 +49,21 @@ def train_reader(settings):
         "do_area_scale": True,
         "scale": [0.08, 1],
         "resize_short_size": 0,
-        "mean": MEAN,
-        "stddev": STDDEV
     }
 
-    manifest_filename = TRAIN_LIST_DIR
-    manifest_root = DATA_DIR
-    cache_dir = CACHE_DIR
-    config = dict()
     config["shuffle_enable"] = True
     config["shuffle_manifest"] = True
-    config['decode_thread_count'] = THREAD
-    config['manifest_filename'] = manifest_filename
-    config['manifest_root'] = manifest_root
-    config['cache_directory'] = cache_dir
-    config['etl'] = [image_config, label_config]
+    config['manifest_filename'] = os.path.join(settings.data_dir, TRAIN_LIST)
     config['augmentation'] = [augmentation_config]
-    config['batch_size'] = settings.batch_size
-    config['iteration_mode'] = "INFINITE"
-
-    #  print(json.dumps(config, indent=4))
+    config['batch_size'] = batch_size
 
     dl = DataLoader(config)
     return dl
 
 
-def val_reader(settings):
-    image_config = {
-        "type": "image",
-        "height": 224,
-        "width": 224,
-        "channels": 3,
-        "output_type": "float",
-        "channel_major": True,
-        "bgr_to_rgb": True
-    }
-
-    label_config = {"type": "label", "binary": False}
+def val_reader(settings, batch_size):
+    config = common_config(settings.cache_dir, settings.data_dir,
+                           settings.reader_thread_count, settings.random_seed)
 
     augmentation_config = {
         "random_seed": 1,
@@ -93,24 +73,52 @@ def val_reader(settings):
         "crop_enable": True,
         "scale": [244.0 / 256.0, 244.0 / 256.0],
         "resize_short_size": settings.resize_short_size,
-        "mean": MEAN,
-        "stddev": STDDEV
     }
 
-    manifest_filename = VAL_LIST_DIR
-    manifest_root = DATA_DIR
-    cache_dir = CACHE_DIR
-
-    config = dict()
-    config['decode_thread_count'] = THREAD
-    config['manifest_filename'] = manifest_filename
-    config['manifest_root'] = manifest_root
-    config['cache_directory'] = cache_dir
-    config['etl'] = [image_config, label_config]
+    config["shuffle_enable"] = False
+    config["shuffle_manifest"] = False
+    config['manifest_filename'] = os.path.join(settings.data_dir, VAL_LIST)
     config['augmentation'] = [augmentation_config]
-    config['batch_size'] = settings.batch_size
-
-    #  print(json.dumps(config, indent=4))
+    config['batch_size'] = batch_size
 
     dl = DataLoader(config)
     return dl
+
+
+def train(settings, batch_size, drop_last=False):
+    # Batch size check
+    batch_size = int(batch_size)
+    if batch_size <= 0:
+        raise ValueError(
+            "batch_size should be a positive integeral value, but got batch_size={}"
+            .format(batch_size))
+    reader = train_reader(settings, batch_size)
+
+    def func():
+        for tup in reader:
+            # tup is (('image',...),('label',...)) where ... stand is data
+            if len(tup[0][1]) == batch_size:
+                yield zip(tup[0][1], tup[1][1])
+        if drop_last == False and len(tup[0][1]) != 0:
+            yield zip(tup[0][1], tup[1][1])
+
+    return func
+
+
+def val(settings, batch_size, drop_last=False):
+    batch_size = int(batch_size)
+    if batch_size <= 0:
+        raise ValueError(
+            "batch_size should be a positive integer value, but got batch_size={}"
+            .format(batch_size))
+    reader = val_reader(settings, batch_size)
+
+    def func():
+        for tup in reader:
+            # tup is (('image',...),('label',...)) where ... stand is data
+            if len(tup[0][1]) == batch_size:
+                yield zip(tup[0][1], tup[1][1])
+        if drop_last == False and len(tup[0][1]) != 0:
+            yield zip(tup[0][1], tup[1][1])
+
+    return func
